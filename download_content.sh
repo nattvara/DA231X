54b547f45440650fc8d1fc30202abc9838fc0542
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-05-02 19:00:26 +0200

    add support for downloading a single file

--- download_content.sh
+++ download_content.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <path_to_google_docs_content>"
-    exit 1
-fi
+download_single_doc() {
+    local doc="$1"
+    local doc_id=$(grep 'doc_id' "$doc" | sed -n 's/.*"doc_id":"\([^"]*\)".*/\1/p')
+    local text_file="content/$(basename "${doc%.*}").tex"
 
-DOCS_PATH="$1"
+    echo "Downloading $doc to $text_file"
+    curl -s "https://docs.google.com/document/d/${doc_id}/export?format=txt" -L -o "$text_file"
+}
 
-if [ ! -d "$DOCS_PATH" ]; then
-    echo "The directory '$DOCS_PATH' does not exist."
-    exit 1
-fi
-
-download_docs() {
-    for doc in "$DOCS_PATH"/*.gdoc; do
+download_docs_in_dir() {
+    local docs_path="$1"
+    for doc in "$docs_path"/*.gdoc; do
         if [ -f "$doc" ]; then
-            doc_id=$(grep 'doc_id' "$doc" | sed -n 's/.*"doc_id":"\([^"]*\)".*/\1/p')
-            text_file="content/$(basename "${doc%.*}").tex"
-
-            echo "Downloading $doc to $text_file"
-            curl -s "https://docs.google.com/document/d/${doc_id}/export?format=txt" -L -o "$text_file"
+            download_single_doc "$doc"
         fi
     done
 }
 
-mkdir -p content
-download_docs
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_google_docs_directory_or_filename>"
+    exit 1
+fi
+
+INPUT="$1"
+
+if [ -d "$INPUT" ]; then
+    mkdir -p content
+    download_docs_in_dir "$INPUT"
+elif [ -f "$INPUT" ]; then
+    mkdir -p content
+    download_single_doc "$INPUT"
+else
+    echo "The input '$INPUT' is neither a valid directory nor a valid file."
+    exit 1
+fi
