9ff2c1d0130e90a467f92e4880aa8cc2f3136f6a
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2024-05-26 22:16:07 +0200

    add function that saves plots to png files with latex code to include them as figures

--- results/notebooks/helper_functions.py
+++ results/notebooks/helper_functions.py
@@ -9,4 +9,30 @@ def save_table(filename: str, latex_code: str):
     with open(fullpath, 'w') as f:
         f.write(latex_code)
 
-    print(f"Saved latex code to {fullpath}")
+    print(f"Saved latex code to {filename}")
+
+
+def save_plot(plt, filename: str, caption: str, label: str):
+    current_file_path = os.path.abspath(__file__)
+    current_dir = os.path.dirname(current_file_path)
+    plot_dir = os.path.join(current_dir, "../plots/assets")
+    latex_dir = os.path.join(current_dir, "../plots")
+
+    plot_fullpath = os.path.realpath(os.path.join(plot_dir, f"{filename}.png"))
+    latex_fullpath = os.path.realpath(os.path.join(latex_dir, f"{filename}.tex"))
+
+    plt.savefig(plot_fullpath, dpi=300)
+
+    latex_code = f"""
+\\begin{{figure}}[H]
+    \\centering
+    \\includegraphics[width=\\textwidth]{{results/plots/assets/{filename}.png}}
+    \\caption{{{caption}}}
+    \\label{{{label}}}
+\\end{{figure}}
+    """.strip()
+
+    with open(latex_fullpath, 'w') as f:
+        f.write(latex_code)
+
+    print(f"Saved plot to {filename}")
